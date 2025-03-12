e63e36030e3dbec0db457ae60d2ec72e3b23d661
king <qiujinwu@gmail.com>
2025-03-12 17:33:06 +0800

    修改client, 增加接口

--- engineio/connect.go
+++ engineio/connect.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/googollee/go-socket.io/engineio/session"
+	"github.com/lixinio/go-socket.io/engineio/session"
 )
 
 // Conn is connection by client session
